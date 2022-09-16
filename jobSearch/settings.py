061ad9c1d227a0241030d6ba930c5240ff0ed05a
merve <mervecolakoglu@outllok.com>
2022-09-16 10:43:04 +0300

    second commit

--- jobSearch/settings.py
+++ jobSearch/settings.py
@@ -92,10 +92,10 @@ WSGI_APPLICATION = 'jobSearch.wsgi.application'
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'jobSearch',
-        'USER': 'postgres',
-       'PASSWORD': '200515',
-        'HOST': 'localhost',
+       'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+       'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
        'PORT':'5432',
    }
 }
